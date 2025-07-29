9748fa472866d72118a1f59e4dd6a1449c7b9be1
TejasMadhukarPatil <102374137+TejasMadhukarPatil@users.noreply.github.com>
2025-07-29 19:03:44 +0530

    Create variables.tf

---
+++ terrafrom/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "dockerhub_username" {
+  default = "yourdockerhub" # Replace with your actual Docker Hub username
+}
